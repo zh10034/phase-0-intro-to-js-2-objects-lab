72ed1a70dcd6df42fb8c8a1fbbec6f8c623caf40
zh10034 <zh10034@LAPTOP-KH5314B2.localdomain>
2023-08-13 20:45:23 -0400

    phase-0-intro-to-js-2-objects-lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = { 
+    name : "Wrigley",
+    streetAddress : "498 Waterford Drive",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]= value
+    return employee;
+};
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee}
+    delete newEmployee[key]
+    return newEmployee;
+};
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+};
+
+
+
+
+
+
